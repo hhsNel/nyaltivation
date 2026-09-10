1d79e50aa79e529ccf6fac82523280f8ea9bf0a6
hhsNel <hihijsonel@gmail.com>
2026-09-10 11:40:45 +0200

    feat:slash commands

--- bot.py
+++ bot.py
@@ -20,12 +20,14 @@ if not TOKEN:
 class TestBot(commands.Bot):
     def __init__(self) -> None:
         intents = discord.Intents.default()
+        intents.message_content = True
         super().__init__(command_prefix="~", intents=intents)
         self.db = Database()
 
     async def setup_hook(self) -> None:
         await self.db.connect()
         await self.add_cog(test_cog(self))
+        await self.tree.sync()
 
     async def close(self) -> None:
         logger.info("Shutting down...")
